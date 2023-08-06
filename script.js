44357381b82c9fd9e0c4f18de07af9869c41a997
dbey21 <125637145+dbey21@users.noreply.github.com>
2023-08-05 17:52:19 -0700

    updated the script.js file

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('hello world')
+const startButton = document.querySelector("#Start-btn");
+const welcomeScreen = document.querySelector("#welcome-screen");
+const quizScreen = document.querySelector("#quiz-screen");
+
+startButton?.addEventListener("click", startGame);
+
+function startGame() {
+  // hide the welcome screen
+
+  welcomeScreen.classList.add("hidden");
+
+  // show the quiz screen
+
+  quizScreen.classList.remove("hidden");
+}
