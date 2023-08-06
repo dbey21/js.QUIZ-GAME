6cfcd6415698fb8fcdb10032069252003d56d4db
dbey21 <125637145+dbey21@users.noreply.github.com>
2023-08-05 18:37:23 -0700

    updated script

--- script.js
+++ script.js
@@ -2,11 +2,48 @@ const startButton = document.querySelector("#Start-btn");
 const welcomeScreen = document.querySelector("#welcome-screen");
 const quizScreen = document.querySelector("#quiz-screen");
 const secondsRemainingEl = document.querySelector("#seconds-remaining");
+const currentQuestionEl = document.querySelector("#current-question");
 const endScreen = document.querySelector("#end-screen");
-let secondsRemaining = 5;
+let secondsRemaining = 10;
+let currentQuestionIndex = 0;
+let score = 0;
 let timer;
 
 startButton?.addEventListener("click", startGame);
+
+const questionsArray = [
+  {
+    prompt: "Which of the following animals primarily lives in water?",
+    options: ["Kangaroo", "Dolphin", "Cheetah", "Ostrich"],
+    correct: 1,
+  },
+  {
+    prompt: "What is the capital of France?",
+    options: ["Berlin", "London", "Madrid", "Paris"],
+    correct: 3,
+  },
+  {
+    prompt: "Which chemical element has the symbol 'Au'?",
+    options: ["Argon", "Gold", "Aluminium", "Silver"],
+    correct: 1,
+  },
+  {
+    prompt: "Which musical instrument has 88 keys?",
+    options: ["Guitar", "Violin", "Trumpet", "Piano"],
+    correct: 3,
+  },
+
+  {
+    prompt: "Who wrote the play 'Romeo and Juliet'?",
+    options: [
+      "William Shakespeare",
+      "Charles Dickens",
+      "Mark Twain",
+      "Jane Austen",
+    ],
+    correct: 0,
+  },
+];
 
 function startGame() {
   // hide the welcome screen
@@ -24,6 +61,42 @@ function startGame() {
 
     secondsRemainingEl.textContent = secondsRemaining;
   }, 1000);
+
+  showNextQuestion();
+}
+
+function showNextQuestion() {
+  var currentQuestion = questionsArray[currentQuestionIndex];
+
+  currentQuestionEl.innerHTML = `
+    <h3>${currentQuestion.prompt}</h3>
+    <div id="answer-buttons">
+      <button class="answer-btn">${currentQuestion.options[0]}</button>
+      <button class="answer-btn">${currentQuestion.options[1]}</button>
+      <button class="answer-btn">${currentQuestion.options[2]}</button>
+      <button class="answer-btn">${currentQuestion.options[3]}</button>
+    </div> 
+    `;
+
+  const answerButtons = document.querySelectorAll(".answer-btn");
+
+  for (let i = 0; i < answerButtons.length; i++) {
+    answerButtons[i].addEventListener("click", () => {
+      evaluateAnswer(i);
+    });
+  }
+}
+
+function evaluateAnswer(i) {
+  var currentQuestion = questionsArray[currentQuestionIndex];
+  const isCorrect = i === currentQuestion.correct;
+
+  if (isCorrect) score++;
+
+  console.log("score: ", score);
+
+  currentQuestionIndex++;
+  showNextQuestion();
 }
 
 function endGame() {
@@ -34,33 +107,3 @@ function endGame() {
   // show the end screen
   endScreen.classList.remove("hidden");
 }
-
-// 1. Which of the following animals primarily lives in water?
-// A. Kangaroo
-// B. Dolphin
-// C. Cheetah
-// D. Ostrich
-
-// 2. What is the capital of France?
-// A. Berlin
-// B. Madrid
-// C. London
-// D. Paris
-
-// 3. Which chemical element has the symbol 'Au'?
-// A. Argon
-// B. Gold
-// C. Aluminium
-// D. Silver
-
-// 4. Which musical instrument has 88 keys?
-// A. Guitar
-// B. Violin
-// C. Trumpet
-// D. Piano
-
-// 5. Who wrote the play "Romeo and Juliet"?
-// A. Charles Dickens
-// B. Mark Twain
-// C. Jane Austen
-// D. William Shakespeare
